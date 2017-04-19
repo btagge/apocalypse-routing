741c2e31468d5b4f155ecd2b4d2c47717cd3341c
Bryan Tagge <btagge@gmail.com>
2017-04-18 20:32:09 -0600

    apocolypse routing update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
-import ApocalypseView from './ApocalypseView'
+import ApocalypseView from './ApocalypseView';
 import './App.css';
 
 class App extends Component {
